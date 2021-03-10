66c511134507246c43e4f678a7fbc4f444517349
lcs-dsmith <dsmith@lcs.on.ca>
2021-03-10 10:25:36 -0500

    Initial Commit

---
+++ SteelCalculator/SteelCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteelCalculatorApp.swift
+//  SteelCalculator
+//
+//  Created by Dylan Smith on 2021-03-10.
+//
+
+import SwiftUI
+
+@main
+struct SteelCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
