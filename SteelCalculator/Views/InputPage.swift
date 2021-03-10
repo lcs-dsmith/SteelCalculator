bc748cd933c124ca98833802ee167e18978abb3d
lcs-dsmith <dsmith@lcs.on.ca>
2021-03-10 10:28:42 -0500

    Made Input view file

---
+++ SteelCalculator/Views/InputPage.swift
@@ -0,0 +1,20 @@
+//
+//  InputPage.swift
+//  SteelCalculator
+//
+//  Created by Dylan Smith on 2021-03-10.
+//
+
+import SwiftUI
+
+struct InputPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputPage_Previews: PreviewProvider {
+    static var previews: some View {
+        InputPage()
+    }
+}
