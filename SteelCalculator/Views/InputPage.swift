b458682d115bafa4d96f3e688985fb6707385170
lcs-dsmith <dsmith@lcs.on.ca>
2021-03-11 15:26:48 -0500

    Button that goes to the output page

--- SteelCalculator/Views/InputPage.swift
+++ SteelCalculator/Views/InputPage.swift
@@ -9,22 +9,41 @@ import SwiftUI
 
 struct InputPage: View {
     
+    @State var goToOutputPage: Bool = false
     @State private var span: String = ""
     @State private var numberOfStories: String = ""
     
     var body: some View {
-        Section(header: Text("What is the distance between columns?")) {
-            TextField("e.g.: 1024", text: $span)
-                .keyboardType(.numberPad)
-                .padding()
-        }
-        Section(header: Text("How many stories?")) {
-            TextField("e.g.: 1024", text: $numberOfStories)
-                .keyboardType(.numberPad)
-                .padding()
+        NavigationView {
+            List {
+                    VStack {
+                        
+                        Section(header: Text("What is the distance between columns?")) {
+                            TextField("e.g.: 1024", text: $span)
+                                .keyboardType(.numberPad)
+                                .padding()
+                        }
+                        Section(header: Text("How many stories?")) {
+                            TextField("e.g.: 1024", text: $numberOfStories)
+                                .keyboardType(.numberPad)
+                                .padding()
+                        }
+                        
+                        NavigationLink(
+                            destination: OutputPage(),
+                            label: {
+                                Text("Calculate")
+                                    .foregroundColor(Color.blue)
+                                    .multilineTextAlignment(.center)
+                                    
+                            })
+                    
+                }
+            }
         }
     }
 }
+
 
 struct InputPage_Previews: PreviewProvider {
     static var previews: some View {
